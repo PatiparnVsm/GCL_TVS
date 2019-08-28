3c3605f8830a5293169ce2b18387c436c6edcaf6
Patiparn Sanguanpao <psanguanpao@hotmail.com>
2019-08-28 20:18:48 +0700

    Add helppage

---
+++ GCL_TVS_API/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GCL_TVS_API.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ GCL_TVS_API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GCL_TVS_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
