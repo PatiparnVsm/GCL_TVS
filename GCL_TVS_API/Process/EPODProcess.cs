9b8ad8a9d0090ab1031cd53b98ad128f276628c6
izerize <kittisak.nsbn@gmail.com>
2019-09-01 11:28:31 +0700

    test push

--- GCL_TVS_API/Process/EPODProcess.cs
+++ GCL_TVS_API/Process/EPODProcess.cs
@@ -1,38 +1,37 @@
 ﻿using GCL_TVS_API.DAL;
 using System;
-using static GCL_TVS_API.Models.EPOD;
-using static GCL_TVS_API.Models.SODetailsService;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace GCL_TVS_API.Process
 {
     public class EPODProcess
     {
-        private static EPODDAL _EPODDal = null;
-        private static EPODDAL EPODDal
-        {
-            get { return (_EPODDal == null) ? _EPODDal = new EPODDAL() : _EPODDal; }
-        }
-
         private static AuthenDAL _AuthenDal = null;
         private static AuthenDAL AuthenDal
         {
             get { return (_AuthenDal == null) ? _AuthenDal = new AuthenDAL() : _AuthenDal; }
         }
-
+        private static EPODDAL _EPODDAL = null;
+        private static EPODDAL EPODDAL
+        {
+            get { return (_EPODDAL == null) ? _EPODDAL = new EPODDAL() : _EPODDAL; }
+        }
         private static Utility _Utility = null;
         private static Utility Utility
         {
             get { return (_Utility == null) ? _Utility = new Utility() : _Utility; }
         }
 
-        public ResponseSODetails GetdataFromJobnoAndSo(RequestJobDetailsFromJobnoAndSo data)
+        public ResponseSODetails GetdataSOFromCustAndSo(RequestSODetailsFromCustAndSo data)
         {
             ResponseSODetails response = new ResponseSODetails();
             try
             {
                 if (Utility.IsGuid(data.TokenID) && AuthenDal.ValidateToken(data.TokenID))
                 {
-                    response.sODetails = EPODDal.GetJobDetailsFromJobnoAndSo(data);
+                    response.sODetails = SODAL.GetSODetailsFromCustAndSo(data);
                     response.responseCode = "00";
                     response.responseMSG = "Success";
                 }
@@ -48,5 +47,6 @@ namespace GCL_TVS_API.Process
             }
             return response;
         }
+
     }
 }
