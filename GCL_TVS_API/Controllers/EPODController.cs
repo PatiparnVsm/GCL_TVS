757fd38bc1cf207382e6e7cba29aa97d376b75b2
Patiparn Sanguanpao <psanguanpao@hotmail.com>
2019-09-01 10:44:17 +0700

    Add EPODController

---
+++ GCL_TVS_API/Controllers/EPODController.cs
@@ -0,0 +1,12 @@
+﻿using GCL_TVS_API.Process;
+using System;
+using System.Web.Http;
+using static GCL_TVS_API.Models.Token;
+
+namespace GCL_TVS_API.Controllers
+{
+    public class EPODController : ApiController
+    {
+       
+    }
+}
