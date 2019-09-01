aed1f3c31cc6041b6b9ae0312afaf2ed01b258b3
izerize <kittisak.nsbn@gmail.com>
2019-09-01 11:13:58 +0700

    GetPicture V0.1

--- GCL_TVS_API/Controllers/EPODController.cs
+++ GCL_TVS_API/Controllers/EPODController.cs
@@ -1,12 +1,51 @@
 ﻿using GCL_TVS_API.Process;
 using System;
 using System.Web.Http;
+using static GCL_TVS_API.Models.Picture;
 using static GCL_TVS_API.Models.Token;
 
 namespace GCL_TVS_API.Controllers
 {
     public class EPODController : ApiController
     {
-       
+        private static GetSODetailsProcess _process = null;
+        private static GetSODetailsProcess process
+        {
+            get { return (_process == null) ? _process = new GetSODetailsProcess() : _process; }
+        }
+
+        [HttpPost]
+        public ResponsePictureSize GetPictureSize([FromBody] RequestPictureSize data)
+        {
+            ResponsePictureSize res = new ResponsePictureSize();
+
+            //try
+            //{
+            //    res = process.GetdataSOFromCustAndSo(data);
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw ex;
+            //}
+
+            return res;
+        }
+
+        //[HttpPost]
+        //public ResponseSODetails GetSoDetailsFromJobnoAndSo([FromBody] RequestSODetailsFromJobnoAndSo data)
+        //{
+        //    ResponseSODetails res = new ResponseSODetails();
+
+        //    try
+        //    {
+        //        res = process.GetdataSOFromJobnoAndSo(data);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+
+        //    return res;
+        //}
     }
 }
---
+++ GCL_TVS_API/Models/PictureList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCL_TVS_API.Models
+{
+    public class PictureList
+    {
+        public Guid TVPictureID { get; set; }
+        public int PictureID { get; set; }
+        public int PictureSequence { get; set; }
+        public int PictureName { get; set; }
+
+    }
+}
