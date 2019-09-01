3219803b182fb52893d387b1ffbcee9e96fc2a60
Patiparn Sanguanpao <psanguanpao@hotmail.com>
2019-09-01 10:47:46 +0700

    add EPOD MODEL,DAL,Process

---
+++ GCL_TVS_API/DAL/EPODDAL.cs
@@ -0,0 +1,17 @@
+﻿using CIMB.DSE.ML.DAL;
+using Dapper;
+using GCL_TVS_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using static GCL_TVS_API.Models.SODetailsService;
+using static GCL_TVS_API.Models.SODetailsUrl;
+
+namespace GCL_TVS_API.DAL
+{
+    public class EPODDAL : BaseConnection
+    {
+        
+    }
+}
---
+++ GCL_TVS_API/Process/EPOD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCL_TVS_API.Process
+{
+    public class EPOD
+    {
+    }
+}
